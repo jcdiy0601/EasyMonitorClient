13b4d2c0154a7d04b336fb94c6758b0ad5286b11
jcdiy0601 <15210942688@163.com>
2018-03-16 15:39:26 +0800

    io

--- plugins/linux/load.py
+++ plugins/linux/load.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # Author: 'JiaChen'
 
+import subprocess
+
 
 def monitor():
-    print('-------------!@#------------')
+    """监控负载"""
+    shell_command = '/usr/bin/uptime'
+    status, result = subprocess.getstatusoutput(cmd=shell_command)
+    if status != 0:
+        value_dict = {'status': status}
+    else:
+        result = result.split()[-3:]
+        load1, load5, load15 = result.split(',')
+        value_dict = {
+            'load1': float(load1),
+            'load5': float(load5),
+            'load15': float(load15),
+            'status': status
+        }
+    return value_dict
+
+if __name__ == '__main__':
+    print(monitor())
