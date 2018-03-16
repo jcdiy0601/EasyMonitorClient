ed3535a8b6028708ec8f354d5a6ccc5ceee7f8ab
jcdiy0601 <15210942688@163.com>
2018-03-16 15:09:11 +0800

    cpu

--- plugins/linux/cpu.py
+++ plugins/linux/cpu.py
@@ -6,3 +6,22 @@ import subprocess
 
 def monitor():
     shell_command = '/usr/bin/sar 1 3 | grep "Average\|平均时间"'
+    status, result = subprocess.getstatusoutput(cmd=shell_command)
+    if status != 0:
+        value_dict = {'status': status}
+    else:
+        user = float(result.split()[2])
+        system = float(result.split()[4])
+        iowait = float(result.split()[5])
+        idle = float(result.split()[-1])
+        value_dict = {
+            'user': user,
+            'system': system,
+            'iowait': iowait,
+            'idle': idle,
+            'status': status
+        }
+    return value_dict
+
+if __name__ == '__main__':
+    print(monitor())
