3b9445e88c294f82a7fd4396bb7b1e8333837cb3
jcdiy0601 <15210942688@163.com>
2018-04-09 17:17:43 +0800

    20180409

--- plugins/linux/network.py
+++ plugins/linux/network.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # Author: 'JiaChen'
 
+import subprocess
+
 
 def monitor():
     """监控网卡流量"""
-    pass
+    shell_command = '/usr/bin/sar -n DEV 1 5 | grep -v IFACE | grep "Average\|平均时间"'
+    result = subprocess.Popen(shell_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.readlines()
+
+
+if __name__ == '__main__':
+    print(monitor())
